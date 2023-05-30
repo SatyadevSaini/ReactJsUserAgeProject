92775b7845b48e54f821a4d89d0c5b7cdaf9b908
Satyadev Saini <105744852+SatyadevSaini@users.noreply.github.com>
2023-05-30 08:44:20 +0530

    Data Sending Using Ref
    Here we will be use ref for getting data from the form instread of useState we will use a new Hook useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './App.css';
 // import FormCreate from './components/FormCreate/FormCreate';
 import PrintData from './components/PrintData/printData';
+
 
 
 const App = () => {
 
   return (
     <div>
+     
       <PrintData/>
     </div>
   );
--- src/components/FormCreate/FormCreate.js
+++ src/components/FormCreate/FormCreate.js
@@ -1,67 +1,94 @@
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./FormCreate.css";
 import CustomAlert from "../../CustomAlert/CustomAlert";;
 
 const FormCreate = (props) => {
-                 
+
   const[currentAge , updatedAge]  = useState("");
   const[currentUsername , updatedUsername]  = useState("");
 
+  const ageByRef = useRef();
+  const NameByRef = useRef();
+
+//   console.log(ageByRef.current.value)
+
+ 
 
     //****************************  get age here  ************************************
-    
+
     const GetAge = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         updatedAge(e.target.value)
     }
 
     //*****************************  get username here   *****************************
-  
+
     const GetUsename = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         updatedUsername(e.target.value);
     }
-    
-   
+
+
 
     const sendValueHere = (event) => {
+        const AGE = ageByRef.current.value;
+        const NAME = NameByRef.current.value;
+      
+        console.log(NAME)
+        console.log(AGE);
+        console.log(AGE);
+        console.log(NAME)
+        
+        
+
         event.preventDefault();
-        if(currentUsername === ""){
+        if(NAME === ""){            
             <CustomAlert message="Error occurred." type="error" />
             console.log("alert");
            alert("Credentail Alert // UserName not Filled !")
-        }else if( currentAge <= 0 || currentAge > 120  ){
+        }else if( AGE <= 0 || AGE > 120  ){
         //  CustomAlert("invalid Credentails")
         <CustomAlert message={"Error occurred."} type={"error"} />
             alert("Invalid Age 0 to 120 Range Allowed only ")
             console.log("alert");
 
         }else{
-            props.sendValue(currentAge , currentUsername);  // this send value function will send value from there ..... 
+            // props.sendValue(currentAge , currentUsername);  // this send value function will send value from there ..... 
+            props.sendValue(AGE , NAME);  // this send value function will send value from there ..... 
+            // ageByRef("")
+            // NameByRef("")
         }
            updatedAge("");
            updatedUsername("");
+
+           ageByRef.current.value="";
+           NameByRef.current.value="";
+
+           //******************reset the REF********************************
+
+
+          
     }
 
 
     return(
-           
+
       <div className="Screen">
          <form onSubmit={sendValueHere}>
          <div className="form_Div" >
          <div className="form_UserName">
-         
+
          <label>Username</label>
          <div>
-            <input type="text" placeholder="USERNAME" value={currentUsername} onChange={GetUsename}/>
+            <input type="text" placeholder="USERNAME" value={currentUsername} onChange={GetUsename} ref={NameByRef}/>
          </div>
           </div>
 
           <div className="form_Age">
          <label>Age(years)</label>
          <div>
-            <input type="number" placeholder="AGE" value={currentAge} onChange={GetAge} />
+            <input type="number" placeholder="AGE" value={currentAge} onChange={GetAge} ref={ageByRef} />
          </div>
           </div>   
                     <div className="add-Button">
